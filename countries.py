29153549450030fbafd2599133f2dc33e33ce38a
Brian Stump <brian.stump@brianstump-ltm.internal.salesforce.com>
2023-01-23 14:28:55 -0500

    added script

---
+++ countries.py
@@ -0,0 +1,29 @@
+import csv, sqlite3
+conn = sqlite3.connect("countries.db")
+c = conn.cursor()
+
+c.execute('DROP TABLE countries;')
+
+c.execute('''
+    CREATE TABLE countries (
+    name TEXT NOT NULL ,
+    alpha TEXT NOT NULL ,
+    code INTEGER NOT NULL ,
+    region TEXT NOT NULL ,
+    intermediate_region TEXT,
+    PRIMARY KEY (code)
+    );
+    ''')
+
+with open('countries.csv','r') as fin:
+    dr = csv.DictReader(fin) 
+    to_db = [(i['name'], i['alpha-3'], i['country-code'], i['region'], i['intermediate-region']) for i in dr]
+
+c.executemany("INSERT INTO countries (name, alpha, code, region, intermediate_region) VALUES (?, ?, ?, ?, ?);", to_db)
+
+c.execute('''
+    SELECT * FROM countries
+    ''')
+
+conn.commit()
+c.close()
